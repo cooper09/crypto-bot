4aee44b40295766d4b23c19ab0bf65b7e3cfd588
cooper09 <cooperatkmba@gmail.com>
2022-04-14 14:21:05 -0400

    transfer funds to Ganache Mainnet receiver account

--- bot-ethers-2.js
+++ bot-ethers-2.js
@@ -3,7 +3,9 @@ const rpcURL = 'http://localhost:8545';
 const provider = new ethers.providers.JsonRpcProvider(rpcURL);
 
 const acct1 = "0x34A2fF8DF9580C977eF00fcE9F33Ac29816F544C";
-const acct2 = "0x00c3e8976ae622C79C6e33749eF999aa9ECba3c1";
+//const acct2 = "0x00c3e8976ae622C79C6e33749eF999aa9ECba3c1";
+//forked mainnet target account
+const acct2 = "0xfFD5F5B573Ac9f6109C07822C74e0c96CbC81848";
 
 //acct 1 is the signer...
 const privateKey1 = '387a0732c908a5ea6aff719ce9c7d552452b52c12cc3b4922461b465784e87b7';
